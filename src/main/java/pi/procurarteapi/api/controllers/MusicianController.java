cbf9545651fe182d4eb194da915d2f272cac7162
RaianNolaco <raiannolaco@gmail.com>
2023-11-14 21:18:52 -0300

    iniciando post images

--- src/main/java/pi/procurarteapi/api/controllers/MusicianController.java
+++ src/main/java/pi/procurarteapi/api/controllers/MusicianController.java
@@ -1,11 +1,16 @@
 package pi.procurarteapi.api.controllers;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -48,6 +53,21 @@ public class MusicianController {
             ShowMusicianResponseDto response = showMusicianService.execute(new ShowMusicianRequestDto(id));
 
             return ResponseEntity.status(HttpStatus.OK).body(response);
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+
+    @PostMapping("{id}/images")
+       public ResponseEntity<?> postImages(@PathVariable String id, @RequestBody ArrayList<String> images) throws Exception {
+        try {
+            
+            ArrayList<String> response; 
+
+            //return ResponseEntity.status(HttpStatus.OK).body(response);
+            return null;
+
 
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
