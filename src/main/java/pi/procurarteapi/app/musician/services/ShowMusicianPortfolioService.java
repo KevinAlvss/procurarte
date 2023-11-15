59867e82fae163c521fed83b5739483c607f4dc7
KevinAlvss <kevin.bernardo64@gmail.com>
2023-11-14 21:12:54 -0300

    feat: add get portfolio

---
+++ src/main/java/pi/procurarteapi/app/musician/services/ShowMusicianPortfolioService.java
@@ -0,0 +1,36 @@
+package pi.procurarteapi.app.musician.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import pi.procurarteapi.app.musician.dtos.ShowMusicianPortfolioRequestDto;
+import pi.procurarteapi.app.musician.dtos.ShowMusicianPortfolioResponseDto;
+import pi.procurarteapi.infra.entities.Musician;
+import pi.procurarteapi.infra.repositories.IMusicianRepository;
+
+@Service
+public class ShowMusicianPortfolioService {
+        
+    @Autowired
+    private IMusicianRepository musicianRepository;
+
+    public ShowMusicianPortfolioService(IMusicianRepository musicianRepository) {
+        this.musicianRepository = musicianRepository;
+    }
+
+    public ShowMusicianPortfolioResponseDto execute(ShowMusicianPortfolioRequestDto request) throws Exception {
+        try {
+
+            Musician musician = musicianRepository.findById(request.getId()).orElseThrow(() -> new Exception("Musician Not Found"));
+
+            ShowMusicianPortfolioResponseDto response = new ShowMusicianPortfolioResponseDto();
+            response.setMusicianId(musician.getId());
+            response.setPortfolio(musician.getPortfolio());
+
+            return response;
+
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+}
