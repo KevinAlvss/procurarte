a1d3a85928972899a155ce47099836f67f4a15d4
Raian Medeiros <raiannolaco@gmail.com>
2023-11-22 01:07:59 -0300

    iniciando testes unitarios

--- src/test/java/pi/procurarteapi/ProcurarteApiApplicationTests.java
+++ src/test/java/pi/procurarteapi/ProcurarteApiApplicationTests.java
@@ -1,11 +1,40 @@
 package pi.procurarteapi;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import pi.procurarteapi.app.musician.dtos.ShowMusician.ShowMusicianRequestDto;
+import pi.procurarteapi.app.musician.dtos.ShowMusician.ShowMusicianResponseDto;
+import pi.procurarteapi.app.musician.services.PostMusicianService;
+import pi.procurarteapi.app.musician.services.ShowMusicianService;
+import pi.procurarteapi.infra.entities.Musician;
+import pi.procurarteapi.infra.entities.Musician.Address;
 
 @SpringBootTest
 class ProcurarteApiApplicationTests {
+	
+	@Autowired
+    private PostMusicianService postMusicianService;
+
+	@Autowired
+    private ShowMusicianService showMusicianService;
+
 	@Test
-	void contextLoads() {
+	void musicianIsEqual() throws Exception {
+
+		Address address = new Address("rua", "sp", "00000-000", "09", "null");
+		Musician musicianPost = new Musician("idMock", "email@mock", "123", "Mock", "11999999999", "@moock", address, null, null, null);
+	
+
+		postMusicianService.execute(musicianPost);
+
+		ShowMusicianRequestDto request = new ShowMusicianRequestDto("idMock"); 
+
+		ShowMusicianResponseDto response = showMusicianService.execute(request); 
+
+		assertThat(musicianPost == response.getMusician()).isTrue();
 	}
 }
