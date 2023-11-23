47f31a441c5fbfee3b0e670d577ce6dc40991c4b
Mariana <mariana0026souza@gmail.com>
2023-11-22 21:00:36 -0300

    fix/deleting-unused-imports

--- src/main/java/pi/procurarteapi/api/controllers/AddressController.java
+++ src/main/java/pi/procurarteapi/api/controllers/AddressController.java
@@ -6,9 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
